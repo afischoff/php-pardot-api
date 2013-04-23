313bc2820f1871566462189736c8913fd81c249f
Andy Fischoff <andy.fischoff@pardot.com>
2013-04-23 16:15:57 -0500

    Basic methods implemented, along with more example code. Error handling and authentication retry as well

--- api.class.php
+++ api.class.php
@@ -14,16 +14,15 @@ class API
 	const URI = 'https://pi.pardot.com/api/';
 	const VERSION = '/version/3';
 	const STAT_SUCCESS = 'ok';
+	const API_EXPIRED_MSG = 'Invalid API key or user key';
 
 	private $email;
 	private $password;
-	private $user_key;
 	private $connection;
 	private $debug;
 	private $logfile;
-	private $format;
-	private $postFields;
-	private $api_key;
+
+	public $postFields;
 
 	/**
 	 * Constructor method for new API wrapper instances
@@ -33,32 +32,168 @@ class API
 	 * @param string $user_key - The API user's user_key, as found in the User Menu > Settings
 	 * @param string $connection - HTTP request handler. cURL is default
 	 * @param bool $debug - When enabled, log messages will be echoed to the screen.
-	 * @param null $logfile - When not null, debug messages will be appended to the file
+	 * @param string $logfile - When not null, debug messages will be appended to the file
 	 */
 	public function __construct($email, $password, $user_key, $connection = 'cURL', $debug = false, $logfile = null)
 	{
 		// store initialization values
 		$this->email = $email;
 		$this->password = $password;
-		$this->user_key = $user_key;
 		$this->connection = $connection;
 		$this->debug = $debug;
 		$this->logfile = $logfile;
-		$this->format = 'json';
 
 		// set default post fields
-		$this->postFields = array('format' => $this->format);
+		$this->postFields = array(
+			'format' => 'json',
+			'user_key' => $user_key
+		);
 
 		// authenticate to get and store the api key
 		$this->authenticate();
 	}
 
 	/**
+	 * Function performs an operation on a single Pardot object as defined by $id or $email
 	 *
+	 * @param string $object - The objects exposed through the API (list, opportunity, prospect, prospectAccount, user, visit, visitor)
+	 * @param string $operation - The operation to perform (read, update, delete, undelete)
+	 * @param int $id - The ID of the object being affected
+	 * @param string $email - The email address of the object being affected
+	 * @param array $parameters - An array of field => value pairs to be set.
+	 * @return array
 	 */
-	public function makeRequest()
+	public function doOperationByIdOrEmail($object, $operation, $id = null, $email = null, $parameters = null)
 	{
+		// setup default return structure
+		$returnStructure = array(
+			'success' => false,
+			'err_msg' => null
+		);
 
+		// validate inputs - $id or $email is required
+		if (is_null($id) && is_null($email))
+		{
+			// debug message
+			$errMsg = 'FATAL: doOperationByIdOrEmail() Invalid input - $id or $email is required';
+			$this->debuglog($errMsg);
+
+			// update return structure
+			$returnStructure['err_msg'] = $errMsg;
+
+			// return
+			return $returnStructure;
+		}
+
+		// build request URL
+		$url = self::URI . $object . self::VERSION . '/do/' . $operation;
+
+		if ( ! is_null($id))
+		{
+			$url .= '/id/' . $id;
+
+		} else {
+
+			$url .= '/email/' . $email;
+		}
+
+		// merge post fields and parameters
+		if (is_array($parameters))
+		{
+			$this->postFields = array_merge($this->postFields, $parameters);
+		}
+
+		// do request and return
+		return $this->makeRequest($url, $this->postFields);
+	}
+
+	/**
+	 * Performs a query on a Pardot object
+	 *
+	 * @param string $object - The objects exposed through the API (list, opportunity, prospect, prospectAccount, user, visit, visitor)
+	 * @param array $parameters - An array of field => value pairs to be set. Also used for limit and offset
+	 * @return array
+	 */
+	public function queryObject($object, $parameters)
+	{
+		// build request URL
+		$url = self::URI . $object . self::VERSION . '/do/query';
+
+		// merge post fields and parameters
+		if (is_array($parameters))
+		{
+			$this->postFields = array_merge($this->postFields, $parameters);
+		}
+
+		// do request and return
+		return $this->makeRequest($url, $this->postFields);
+	}
+
+	/**
+	 * Makes all API requests automatically authenticating if the api_key expires.
+	 *
+	 * @param $url
+	 * @param $postFields
+	 * @return array
+	 */
+	public function makeRequest($url, $postFields = null)
+	{
+		// setup default return structure
+		$returnStructure = array(
+			'success' => false,
+			'response' => null
+		);
+
+		// try api request
+		$resp = $this->sendPostRequest($url, $postFields);
+
+		// return if response is invalid
+		if ( ! $resp['success'])
+		{
+			return $returnStructure;
+
+		} else {
+
+			// if response is successful, return. Else if api_key expired, authenticate and try again
+			if ($resp['resp_decoded']->{'@attributes'}->stat == self::STAT_SUCCESS)
+			{
+				// update return structure
+				$returnStructure['success'] = true;
+				$returnStructure['response'] = $resp['resp_decoded'];
+
+				// return
+				return $returnStructure;
+
+			} else if ($resp['resp_decoded']->err == self::API_EXPIRED_MSG) {
+
+				// API key expired - try authenticating again
+				if ( ! $this->authenticate() )
+				{
+					// return
+					return $returnStructure;
+				}
+
+				// try api request again
+				$resp = $this->sendPostRequest($url, $postFields);
+
+				// if response is successful, return
+				if ($resp['resp_decoded']->{'@attributes'}->stat == self::STAT_SUCCESS)
+				{
+					// update return structure
+					$returnStructure['success'] = true;
+					$returnStructure['response'] = $resp['resp_decoded'];
+
+					// return
+					return $returnStructure;
+
+				} else {
+
+					// return
+					return $returnStructure;
+				}
+
+			}
+		}
 	}
 
 	/**
@@ -68,14 +203,13 @@ class API
 	 */
 	private function authenticate()
 	{
-		// form URL
+		// build request URL
 		$url = self::URI . 'login' . self::VERSION;
 
 		// compile post fields
 		$postFields = $this->postFields;
 		$postFields['email'] = $this->email;
 		$postFields['password'] = $this->password;
-		$postFields['user_key'] = $this->user_key;
 
 		// debug messages
 		$this->debuglog('Trying to authenticate...');
@@ -84,13 +218,15 @@ class API
 		$resp = $this->sendPostRequest($url, $postFields);
 
 		// if successful, store the api key and return
-		if ($resp['resp_decoded']->{'@attributes'}->stat == self::STAT_SUCCESS && $resp['resp_decoded']->api_key)
+		if ($resp['success']
+			&& $resp['resp_decoded']->{'@attributes'}->stat == self::STAT_SUCCESS
+			&& $resp['resp_decoded']->api_key)
 		{
 			// debug messages
-			$this->debuglog('Authentication Successful! Api_key received: ' . $resp['resp_decoded']->api_key);
+			$this->debuglog('Authentication Successful!');
 
 			// store api key
-			$this->api_key = $resp['resp_decoded']->api_key;
+			$this->postFields['api_key'] = $resp['resp_decoded']->api_key;
 
 			return true;
 
@@ -100,7 +236,7 @@ class API
 			$this->debuglog('Authentication Failed!');
 
 			// unset api key
-			$this->api_key = null;
+			unset($this->postFields['api_key']);
 
 			return false;
 		}
@@ -111,9 +247,9 @@ class API
 	 *
 	 * @param string $url - The complete URL to be requested
 	 * @param array $postFields - Post fields in array form
-	 * @return mixed -
+	 * @return array return structure
 	 */
-	private function sendPostRequest($url, $postFields)
+	private function sendPostRequest($url, $postFields = null)
 	{
 		// setup default return structure
 		$returnStructure = array(
@@ -123,10 +259,26 @@ class API
 			'resp_decoded' => null
 		);
 
+		// use default post fields if none are passed in
+		if (is_null($postFields))
+		{
+			$postFields = $this->postFields;
+		}
+
 		// build query string from $postFields
 		$postFieldsString = '';
+
 		foreach ($postFields as $field => $value)
 		{
+			// handle boolean values
+			if ($value === true)
+			{
+				$value = 'true';
+			} else if ($value === false) {
+				$value = 'false';
+			}
+
+			// append to string
 			$postFieldsString .= $field . '=' . urlencode($value) . '&';
 		}
 		$postFieldsString = substr($postFieldsString, 0, -1);
@@ -152,6 +304,19 @@ class API
 				$returnStructure['resp_body'] = curl_exec($ch);
 				$returnStructure['resp_code'] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
+				// if an error happened, log and return
+				if ($returnStructure['resp_body'] === false)
+				{
+					// log error message
+					$this->debuglog('FATAL cURL error: ' . curl_error($ch));
+
+					// close connection
+					curl_close($ch);
+
+					// return
+					return $returnStructure;
+				}
+
 				// close connection
 				curl_close($ch);
 
@@ -163,7 +328,7 @@ class API
 		$this->debuglog('Response body received: ' . $returnStructure['resp_body']);
 
 		// convert response to data object
-		switch ($this->format)
+		switch ($postFields['format'])
 		{
 			case 'json':
 				// decode JSON string
--- example.php
+++ example.php
@@ -7,5 +7,24 @@ spl_autoload_register(function ($class) {
 });
 
 // instantiate new Pardot client
-$pardot_client = new \Pardot\API('andy.fischoff+api@pardot.com', 'Pardot07!23', '3c177b33f38d3150ebddb4f9d6ff36c8', 'cURL', true);
+$pardot_client = new \Pardot\API('andy.fischoff+api@pardot.com', 'Pardot07!23', '3c177b33f38d3150ebddb4f9d6ff36c8');
 
+// if not authenticated, stop here.
+if ( empty($pardot_client->postFields['api_key']) )
+{
+	die('Pardot Authentication Failed!');
+}
+
+// read prospect by id
+$prospect = $pardot_client->doOperationByIdOrEmail('prospect', 'read', 63045632);
+echo 'Prospect:';
+var_dump($prospect);
+
+// query prospects
+$prospectSearch = $pardot_client->queryObject('prospect', array(
+	'assigned' => true,
+	'score_greater_than' => 100,
+	'limit' => 4
+));
+echo 'Prospect Search:';
+var_dump($prospectSearch);
