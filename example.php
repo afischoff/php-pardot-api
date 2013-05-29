eff30faa6627e95e7867306de3f18e20c3a13742
Andy Fischoff <andy.fischoff@pardot.com>
2013-05-28 22:04:35 -0500

    example updates

--- example.php
+++ example.php
@@ -8,14 +8,11 @@ spl_autoload_register(function ($class) {
 
 // namespace config
 use \Pardot\API as API;
-/*
+
 // read email by id
 echo 'Get email by ID:' . "\n";
 $email = API::Instance()->doOperationById(API::OBJ_EMAIL, API::OPR_READ, 447822116);
 var_dump($email['response']->email);
-
-echo "\n\n";
-
 
 // read prospect by email
 echo 'Get prospect by email:' . "\n";
@@ -24,7 +21,7 @@ $prospect = API::Instance()->doOperationByField(API::OBJ_PROSPECT,
 								'email',
 								'email@example.com');
 var_dump($prospect);
-*/
+
 // read all prospects updated in the last 15 minutes
 $prospects = API::Instance()->queryObject(API::OBJ_PROSPECT, array('updated_after' => '15 minutes ago'));
 var_dump($prospects);
