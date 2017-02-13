7f2fec31f1919572e1527b66955391f865ac8445
Whitt <cwhittle@zweiggroup.com>
2017-02-13 12:50:32 -0600

    added back old examples

--- example.php
+++ example.php
@@ -14,6 +14,21 @@ $pardot_config = new \Pardot\Config(
     )
 );
 
+// read email by id
+echo 'Get email by ID:' . "\n";
+$email = API::Instance($pardot_config)->doOperationById(API::OBJ_EMAIL, API::OPR_READ, 447822116);
+var_dump($email['response']->email);
+
+// read prospect by email
+echo 'Get prospect by email:' . "\n";
+$prospect = API::Instance($pardot_config)->doOperationByField(
+    API::OBJ_PROSPECT,
+    API::OPR_READ,
+    'email',
+    'email@example.com'
+);
+var_dump($prospect);
+
 // read all prospects updated in the last 15 minutes
 $prospects = API::Instance($pardot_config)->queryObject(API::OBJ_PROSPECT, array('updated_after' => '15 minutes ago'));
 var_dump($prospects);
